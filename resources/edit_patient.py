cdf8d4f0b44b26a7b932acbe94b3c383679d7302
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-05-25 18:12:53 -0300

    Refatorando edição de um paciente

--- resources/edit_patient.py
+++ resources/edit_patient.py
@@ -4,10 +4,16 @@ from flask_jwt_extended import jwt_required
 from models.patient import PatientModel
 from datetime import datetime
 
+
 class EditPatient(Resource):
 
     parser = reqparse.RequestParser()
     parser.add_argument('name',
+                        type=str,
+                        required=False,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('status',
                         type=str,
                         required=False,
                         help="This field cannot be blank."
@@ -18,7 +24,12 @@ class EditPatient(Resource):
                         help="This field cannot be blank."
                         )
     parser.add_argument('number',
-                        type=int,
+                        type=str,
+                        required=False,
+                        help="This field cannot be blank."
+                        )
+    parser.add_argument('telephone_type',
+                        type=str,
                         required=False,
                         help="This field cannot be blank."
                         )
@@ -54,6 +65,7 @@ class EditPatient(Resource):
                         type=str,
                         required=False
                         )
+
     def put(self, id):
         data = EditPatient.parser.parse_args()
 
@@ -61,23 +73,56 @@ class EditPatient(Resource):
 
         if patient:
             if data['name']:
-                patient.person_pat.name = data['name']
+                if len(data['name']) <= 1:
+                    return {"message": "Type a valid name"}
+                patient.PERSON.name = data['name']
+
             if data['email']:
-                patient.person_pat.email = data['email']
+                patient.PERSON.email = data['email']
+
             if data['number']:
-                patient.person_pat.telephones[0].number = data['number']
+                if len(data['number']) > 15 or len(data['number']) < 8 or not data['number'].isdigit():
+                    return {"message": "Type a valid telephone_number"}
+                patient.PERSON.telephones[0].number = data['number']
+
+            if data['telephone_type']:
+                if str(data['telephone_type'].lower()) != str("residencial") and str(
+                        data['telephone_type'].lower()) != str(
+                        "pessoal") \
+                        and str(data['telephone_type'].lower()) != str("comercial"):
+                    return {"message": "Type a valid telephone_type"}
+                patient.PERSON.telephones[0].telephone_type = data['telephone_type']
+
+            if data['status']:
+                if str(data['status'].lower()) != str("andamento") and str(data['status'].lower()) != str(
+                        "aguardando") \
+                        and str(data['status'].lower()) != str("finalizado"):
+                    return {"message": "Type a valid status"}
+                patient.status = data['status']
+
             if data['scholarity']:
                 patient.scholarity = data['scholarity']
+
             if data['observation']:
                 patient.observation = data['observation']
+
             if data['manual_domain']:
+                if str(data['manual_domain'].lower()) != str("destro") and str(data['manual_domain'].lower()) != str(
+                        "canhoto"):
+                    return {"message": "Type a valid manual_domain"}
                 patient.manual_domain = data['manual_domain']
+
             if data['registry_number_pat']:
                 patient.registry_number_pat = data['registry_number_pat']
+
             if data['kinship_degree']:
-                patient.accountables.kinship_degree = data['kinship_degree']
+                patient.ACCOUNTABLE.kinship_degree = data['kinship_degree']
+
             if data['registry_number_acc']:
-                patient.accountables.registry_number_acc = data['registry_number_acc']
+                return {"message": "You cannot change this"}
+
+            if data['date_of_birth']:
+                patient.dt_birth = data['date_of_birth']
         else:
             return {'message': 'User not found.'}, 404
 
