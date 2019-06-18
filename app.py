8bf5480fe97cdfbcdb13594c0c8cbd49f72b6dc0
Daniel Sousa <daniel_sousa.unb@outlook.com>
2019-06-18 20:19:55 -0300

    Ajustando erros
    Co-authored-by: filypsdias <lipao.dias@hotmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from db import db
 
 from resources.create_patient import RegisterPatient
 from resources.pat_information import ShowAllInformationPatient, ShownPatientInformationID
@@ -21,19 +22,12 @@ app.config['JWT_SECRET_KEY'] = os.environ.get("APP_SECRET_KEY")
 api = Api(app)
 CORS(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
-jwt = JWTManager(app)
-
 api.add_resource(RegisterPatient, '/register-patient')
 api.add_resource(ShowAllInformationPatient, '/list-patients/<string:crp>')
 api.add_resource(ShownPatientInformationID, '/patient-information/<int:id>')
 api.add_resource(EditPatient, '/edit-patient/<int:id>')
 
+db.init_app(app)
+
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(debug=True, host='0.0.0.0', port='5001')
