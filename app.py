741a7c24cbb5690e9645d54a095b362826eb7590
filypsdias <lipao.dias@hotmail.com>
2019-06-18 21:47:33 -0300

    Inserindo arquivos para realizacao do deploy
    Co-authored-by: dansousamelo <daniel_sousa.unb@outlook.com>
    Co-authored-by deborapvilela <deborapvilela@gmail.com>

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ api.add_resource(EditPatient, '/edit-patient/<int:id>')
 db.init_app(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5001')
+    app.run(debug=True, host='0.0.0.0')
